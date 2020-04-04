99a88c146d35a947a3d2ad7fd4a47a49427929e2
stackpond <stackpond@gmail.com>
2020-04-04 17:47:50 +1100

    Added flags

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-      <router-link class="navbar-brand" to="/">NCBI Explorer</router-link>
+    <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
+      <span class="navbar-brand">{{$store.state.main.appName}}</span>
       <button
         class="navbar-toggler"
         type="button"
@@ -17,12 +17,38 @@
             type="button"
             value="Search"
             class="btn btn-outline-success my-2 my-sm-0"
-            @click="search()"
+            @click="search(true)"
           />
         </div>
       </div>
     </nav>
-    <router-view></router-view>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+          <input type="text" placeholder="Search" class="form-control" v-model="filterTerm" />
+        </div>
+      </div>
+      <div class="row m-1">
+        <div
+          v-for="(document, documentIndex) of documents"
+          :key="documentIndex"
+          class="card mt-1 col-12"
+        >
+          <div class="card-body">
+            <h6 class="card-title">PMID# {{document.id}}</h6>
+            <h5 class="card-title">{{document.title}}</h5>
+            <hr />
+            <div class="card-text text-justify">{{document.abstract}}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <nav v-show="documents.length > 0" class="navbar fixed-bottom navbar-light navbar-dark bg-dark">
+      <div class="btn btn-outline-success m-1" @click="search(false)">Load more</div>
+      <span
+        class="navbar-brand"
+      >Loaded total of {{$store.getters.totalDocumentsLoaded}} of {{$store.getters.documentCount}} documents</span>
+    </nav>
   </div>
 </template>
 
@@ -31,14 +57,34 @@ export default {
   name: "App",
   data() {
     return {
+      filterTerm: "",
       searchTerm: ""
     };
   },
+  computed: {
+    documents() {
+      if (this.filterTerm !== "") {
+        return this.$store.getters.documents.filter(
+          d => d.id.includes(this.filterTerm)
+        );
+      } else {
+        return this.$store.getters.documents;
+      }
+    }
+  },
   methods: {
-    search() {
-      this.$router.push({ name: "search", params: { searchTerm: this.searchTerm } });
-      this.$router.go();
+    search(reset) {
+      this.$store.dispatch("search", {
+        searchTerm: this.searchTerm,
+        reset: reset
+      });
     }
   }
 };
 </script>
+<style>
+body {
+  padding-top: 70px;
+  padding-bottom: 70px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Vue from 'vue'
-import router from './router';
 
 import App from './App.vue'
+import store from './store'
 
 Vue.config.productionTip = false
 
 
 new Vue({
-  router,
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
